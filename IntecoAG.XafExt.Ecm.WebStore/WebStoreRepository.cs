835984f5de0e1ff7c1792b126b761c5f663a0c65
Pavel Uzorin <puzorin@yandex.ru>
2020-04-30 00:30:21 +0300

    WebStoreRepository Add Generalization

--- IntecoAG.XafExt.Ecm.WebStore/WebStoreRepository.cs
+++ IntecoAG.XafExt.Ecm.WebStore/WebStoreRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.IO;
+
+using DevExpress.Xpo;
 
 namespace IntecoAG.XafExt.Ecm.WebStore {
 
-    public class WebStoreRepository {
+    [MapInheritance(MapInheritanceType.ParentTable)]
+    public class WebStoreRepository: EcmRepository {
 
-        
+        public WebStoreRepository(Session session) : base(session) { }
+
+        public override void AddContent(EcmDocument doc, Stream stream) {
+            throw new NotImplementedException();
+        }
 
     }
 
