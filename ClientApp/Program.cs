a81e0ea69d19e5b847ccd1f699398381a3060708
Uzorin Pavel <puzorin@yandex.ru>
2020-05-09 17:01:04 +0300

    клиент с получением контента

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -9,6 +9,7 @@ namespace ClientApp
     {
         static void Main(string[] args)
         {
+
             string baseUrl = "http://localhost:5000";
             System.Net.Http.HttpClient httpClient = new HttpClient();
 
@@ -17,17 +18,18 @@ namespace ClientApp
 
             var docDto = new DocDTO() { Cmisaction = "create" };
 
-            var guid = Guid.Parse("06ace423-001a-42a8-9cfe-8c63d4068f9a");
+
             //var result = client.Put(guid, content);
             var document = client.Post(docDto);
-            // document.Wait();
+            document.Wait();
             var putResult = client.Put(document.Result.Id, content);
+            putResult.Wait();
             // var getDocRes = client.GetDocument(document.Result.Id);
             var getContentRes = client.GetContent(document.Result.Id);
-            //var r= getContentRes.Result;
             getContentRes.Wait();
-            getContentRes.Result.Stream.CopyTo(new FileStream(@"C:\Projects\lizon.txt", FileMode.OpenOrCreate));
-            //result.Wait();
+            //var r= getContentRes.Result;
+            StreamReader sr = new StreamReader(getContentRes.Result.Stream);
+            var str = sr.ReadToEnd();
         }
     }
 }
